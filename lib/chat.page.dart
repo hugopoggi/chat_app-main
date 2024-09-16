cc4ab2855b4ea3efa70718aae6b897f321dacd8b
Hugo Vinicius Poggi <hugopoggiluciano@gmail.com>
2024-09-16 20:33:43 -0300

    feat implementing chat page with firebase stora

--- lib/chat.page.dart
+++ lib/chat.page.dart
@@ -1,12 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget {
+  var txtMessage = TextEditingController();
+
   var user = (FirebaseAuth.instance).currentUser;
 
   void signOut(BuildContext context) {
     (FirebaseAuth.instance).signOut();
     Navigator.pushReplacementNamed(context, '/login');
+  }
+
+  void sendMessage() {
+    var data = {
+      "user_id": user!.uid,
+      "name": user!.displayName,
+      "message": txtMessage.text,
+      "date": DateTime.now()
+    };
+
+    FirebaseFirestore.instance.collection('messages').add(data);
+
+    txtMessage.clear();
   }
 
   @override
@@ -17,7 +33,7 @@ class ChatPage extends StatelessWidget {
         actions: [
           IconButton(
             onPressed: () => signOut(context),
-            icon: const Icon(Icons.logout),
+            icon: Icon(Icons.logout),
           )
         ],
       ),
@@ -25,26 +41,34 @@ class ChatPage extends StatelessWidget {
         children: [
           Flexible(
             child: Container(
-              child: ListView(
-                reverse: true,
-                children: const [
-                  ListTile(
-                    leading: CircleAvatar(
-                      child: Text("N"),
-                    ),
-                    title: Text("Nome do Usuário"),
-                    subtitle: Text("Oi, tudo bem?"),
-                  ),
-                ],
-              ),
+              child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+                  stream: FirebaseFirestore.instance
+                      .collection("messages")
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) return CircularProgressIndicator();
+
+                    return ListView(
+                      reverse: true,
+                      children: snapshot.data!.docs
+                          .map((doc) => ListTile(
+                            key: Key(doc.id),
+                            leading: CircleAvatar(),
+                            title: Text(doc["nome"]),
+                            subtitle: Text(doc["message"]),
+                          ))
+                          .toList(),
+                    );
+                  }),
             ),
           ),
           Row(
             children: [
               Expanded(
                 child: Container(
-                  padding: const EdgeInsets.all(4),
-                  child: const TextField(
+                  padding: EdgeInsets.all(4),
+                  child: TextField(
+                    controller: txtMessage,
                     decoration: InputDecoration(
                       border: OutlineInputBorder(),
                       hintText: "Digite sua mensagem...",
@@ -53,8 +77,8 @@ class ChatPage extends StatelessWidget {
                 ),
               ),
               IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.send),
+                onPressed: sendMessage,
+                icon: Icon(Icons.send),
               )
             ],
           )
